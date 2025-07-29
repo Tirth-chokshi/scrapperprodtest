8d2b1b34d59d3100caf65ce1cf171d8cbdcb2aa9
Tirth chokshi <chokshitirth4@gmail.com>
2025-07-29 12:11:36 +0530

    minor change

--- src/lib/extractLyrics.js
+++ src/lib/extractLyrics.js
@@ -6,7 +6,6 @@ export default async function extractLyrics(url) {
     let { data } = await axios.get(url);
     const $ = cheerio.load(data);
     let lyrics = $('div[class="lyrics"]').text().trim();
-
     if (!lyrics) {
       lyrics = "";
       $('div[class^="Lyrics__Container"]').each((i, elem) => {
@@ -19,24 +18,9 @@ export default async function extractLyrics(url) {
         }
       });
     }
-
-    if (!lyrics) {
-      // Try additional selectors for newer Genius pages
-      $('div[data-lyrics-container="true"]').each((i, elem) => {
-        if ($(elem).text().length !== 0) {
-          let snippet = $(elem)
-            .html()
-            .replace(/<br>/g, "\n")
-            .replace(/<(?!\s*br\s*\/?)[^>]+>/gi, "");
-          lyrics += $("<textarea/>").html(snippet).text().trim() + "\n\n";
-        }
-      });
-    }
-
     if (!lyrics) return null;
     return lyrics.trim();
-  } catch (error) {
-    console.error("Error extracting lyrics:", error);
-    throw new Error("Failed to extract lyrics");
+  } catch (e) {
+    throw e;
   }
 }
